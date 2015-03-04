0f14901247aa7ba9e4b8edf37bd5cd8dac67afc8
Roman <roman.atachiants@gmail.com>
2015-03-04 20:27:49 +0000

    make sure we have s3cmd to download things

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,14 @@
 # AWS_SECRET_KEY: The secret key to use for S3
 
 # Move S3 Configuration file
+apt-get install -y s3cmd
 cp .s3cfg ~/.s3cfg
 
 # Download the package
 s3cmd get s3://$APP_BUCKET/$APP_FILE
 
 # Unzip the file and delete the zip
-unzip ${APP_FILE} -d /app
+unzip $(find /app -name "*.zip") -d /app
 rm -rf ${APP_FILE}
 
 # Run the application
