7cd03c8a1a110dba04713dfcbd42c69f3238b2e4
Roman <roman.atachiants@gmail.com>
2015-03-05 14:22:26 +0000

    run

--- deploy.sh
+++ deploy.sh
@@ -18,12 +18,12 @@ apt-get install -y libgdiplus
 # Download the package
 s3cmd get s3://$APP_BUCKET/$APP_FILE
 
-# Unzip the file and delete the zip
+# Unzip the package
 unzip $(find /app -name "*.zip") -d /app
-rm -rf ${APP_FILE}
 
 # Tuning GC
 export MONO_GC_PARAMS="soft-heap-limit=512m,nursery-size=128m,major=marksweep-conc,minor=split"
 
-# Run the application
+# Precompile & run the application
 mono --aot -O=all ${APP_ENTRY}
+mono ${APP_ENTRY}
